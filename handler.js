72dc4a75d607baf33b4ba00718fcf6e76284ffa1
Eloff <dan.eloff@gmail.com>
2017-07-19 08:53:36 -0700

    Add package.json

--- handler.js
+++ handler.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const YAML = require('yamljs');
+
 module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
@@ -14,3 +16,35 @@ module.exports.hello = (event, context, callback) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+module.exports.foo = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'foo',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
+
+module.exports.bar = (event, context, callback) => {
+  const config = YAML.load("serverless.yml");
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'bar: ' + config.length,
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
+
