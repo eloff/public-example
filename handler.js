88d4f7d9fd5d0020f64ccdbe61c527e7a732ec10
Eloff <dan.eloff@gmail.com>
2017-07-28 16:04:53 -0700

    Fixes for openwhisk.

--- handler.js
+++ handler.js
@@ -2,49 +2,10 @@
 
 const YAML = require('yamljs');
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Public example. Your function executed successfully!',
-      input: event,
-    }),
+module.exports.hello = (params) => {
+  return {
+    message: 'Public example. Your function executed successfully!',
+    haveYAML: !!YAML
   };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
-
-module.exports.foo = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'foo',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
-
-module.exports.bar = (event, context, callback) => {
-  const config = YAML.load("serverless.yml");
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'bar: ' + config.length,
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
 
