bf6677d218166b57d0321feadb9b8dd81ca4d52c
Eloff <dan.eloff@gmail.com>
2017-07-31 10:08:37 -0700

    Remove yamljs dep.

--- handler.js
+++ handler.js
@@ -1,11 +1,8 @@
 'use strict';
 
-const YAML = require('yamljs');
-
 module.exports.hello = (params) => {
   return {
     message: 'Public example. Your function executed successfully!',
-    haveYAML: !!YAML
   };
 };
 
